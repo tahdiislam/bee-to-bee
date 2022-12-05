7bad02948a6114f4213cfe14b9896bfabad86c8f
tahdi-islam <tshdi60@gmail.com>
2022-12-05 21:35:40 +0600

    create bee to bee

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { RouterProvider } from 'react-router-dom'
+import './App.css'
+import { router } from './Router/router'
+
+function App() {
+
+  return (
+    <div className="App">
+      <RouterProvider router={router}/>
+    </div>
+  )
+}
+
+export default App
